0acb96466edadfb896d30409aa3d9f71b8c42a07
teterabOb <gilbertsahumada@gmail.com>
2021-10-19 21:08:09 -0300

    testing with mocha will be replaced for hardhat

--- test/piggybank.js
+++ test/piggybank.js
@@ -1,26 +1,34 @@
-const PiggyBank = artifacts.require("PiggyBank.sol");
+//const PiggyBank = require("PiggyBank.sol");
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 
 const web3 = new Web3(ganache.provider());
 
+let piggyBank;
+
+beforeEach("setup contract for each test", async() => {
+    piggyBank = await PiggyBank.new();
+});
 
 contract("PiggyBank", accounts => {
-    let piggyBank;
-
-    beforeEach("setup contract for each test", async() => {
-        piggyBank = await PiggyBank.new();
+    it("should be deployed", () => {
+        assert.ok(piggyBank.address)
     });
-
+    
     it("should has a owner", async() => {
         assert.equal(await piggyBank.owner(), accounts[0]);
     });
 
-    it("should accepts funds", async() => {
-        await piggyBank.sendTransaction({value: web3.utils.toWei('2', 'ether'), from: accounts[1]});
+    it("should accepts funds", async() => {        
+        await piggyBank.send(web3.utils.toWei('2', 'ether'), accounts[1]);
 
         const piggyBankAddress = await piggyBank.address;
-        assert.equal(web3.eth.getBalance(piggyBankAddress), web3.utils.toWei('2', 'ether'));
+        let piggyBalance = web3.eth.getBalance(piggyBankAddress)
+        let accountBalance = web3.eth.getBalance(accounts[0])
+        console.log("piggyBalance");
+        //assert.equal(web3.eth.getBalance(piggyBankAddress), web3.utils.toWei('2', 'ether'));
+        assert(piggyBalance == 0);
+     
     });
 
     it("should be owner to withdraw amount", async () => {
