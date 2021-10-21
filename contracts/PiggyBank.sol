2dac2c31475c4a1e5f19592ef8f5debdcf471a7b
teterabOb <gilbertsahumada@gmail.com>
2021-10-21 19:22:33 -0300

    test piggy bannk dopne

--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PiggyBank is Ownable{
     
